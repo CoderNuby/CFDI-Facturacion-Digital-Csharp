aa7acbaa52b9c69282697b54193ee76014bf95cf
CoderNuby <correofake12a@gmail.com>
2020-06-22 04:12:19 -0700

    first commit. se creo codigo para generar un xml NO VALIDO PARA EL SAT. implementacion de una clase llamada 'Comprobante' que cuenta con la mayoria del catalogo del sat

---
+++ FacturacionDigital-CFDI/PFX.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace FacturacionDigital_CFDI
+{
+    
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public class PFX
+    {
+        string cer;
+        string key;
+        string clavePrivada = "";
+        string archivoPFX;
+        string archivoKPEM;
+        string archivoCPEM;
+
+        public string error = "";
+        public string mensajeExito = "";
+
+
+
+        public PFX(string cer_, string key_, string clavePrivada_, string archivoPfx_, string pathTemp)
+        {
+            cer = cer_;
+            key = key_;
+            clavePrivada = clavePrivada_;
+
+            archivoKPEM = pathTemp + "kpem.pem";
+            archivoCPEM = pathTemp + "cpem.pem";
+
+            archivoPFX = archivoPfx_;
+        }
+
+
+        public bool crearPFX()
+        {
+            bool exito = false;
+
+
+            //Validacion
+            if (!File.Exists(cer))
+            {
+                error = "No existe el archivo cer en el sistema";
+                return false;
+            }
+
+            if (!File.Exists(key))
+            {
+                error = "No existe el archivo key en el sistema";
+                return false;
+            }
+
+            if (clavePrivada.Trim().Equals(""))
+            {
+                error = "No existe una clave privada aun en el sistema";
+                return false;
+            }
+
+            //Creamos objetos process
+            Process proc1 = new Process();
+            Process proc2 = new Process();
+            Process proc3 = new Process();
+
+            proc1.EnableRaisingEvents = false;
+            proc2.EnableRaisingEvents = false;
+            proc3.EnableRaisingEvents = false;
+
+            //openssl x509 -inform DER  -in certificado.cer -out certificado.pem
+            proc1.StartInfo.FileName = "openss1";
+            /*
+             CODIGO PENDIENTE
+             https://www.youtube.com/watch?v=qu9kNr63DJs&list=PLWYKfSbdsjJiQwcGNZmICkes0iJHN_7XJ&index=2
+             VER VIDEO DE YOUTUBE PARA VER MAS ACERCA DE LA IMPLEMENTACION DE ESTE MENTODO
+
+             */
+            return false;//QUITAR AL IMPLEMENTAR OPENSSL!!!!!!!!!!!!!!
+        }
+    }
+
+}
---
+++ FacturacionDigital-CFDI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+namespace FacturacionDigital_CFDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comprobante compr = new Comprobante();
+
+            compr.Folio = "666";//Numero de factura
+
+            ////////EMISOR////////////////////////////////////
+            ComprobanteEmisor emisor = new ComprobanteEmisor();
+
+            emisor.Rfc = "POWE870601DM7";
+            emisor.Nombre = "una razón";
+            emisor.RegimenFiscal = c_RegimenFiscal.Item605;
+
+            ////////RESEPTOR////////////////////////////////////
+            ComprobanteReceptor receptor = new ComprobanteReceptor();
+            receptor.Nombre = "Test";
+            receptor.Rfc = "TEST1234566546";
+
+            compr.Emisor = emisor;
+
+            compr.Receptor = receptor;
+
+
+            //Serializamos////////////////////////////////////////////////////////
+            string pathXML = @"C:\Users\ivanr\Documents\github\FacturacionDigital-CFDI\FacturacionDigital-CFDI\xml\myFirstXML.xml";
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Comprobante));
+
+            string Sxml = "";
+
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    xmlSerializer.Serialize(writer, compr);
+                    Sxml = sww.ToString();
+                }
+            }
+
+            ///////GUARDAMOS EL STRING EN UN ARCHIVO//////////////////////////
+
+            File.WriteAllText(pathXML, Sxml);
+
+
+        }
+    }
+}
